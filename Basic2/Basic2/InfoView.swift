0419aa39a419115db4908feca80d80ee3322fa74
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2022-03-21 21:35:01 +0900

    added separate subview file

---
+++ Basic2/Basic2/InfoView.swift
@@ -0,0 +1,34 @@
+//
+//  InfoView.swift
+//  Basic2
+//
+//  Created by YJ on 2022/03/21.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    let info: String
+    let iconName: String
+    
+    var body: some View {
+        Capsule()
+            .fill(.white)
+            .frame(height: 40)
+            .overlay(
+                HStack {
+                    Image(systemName: iconName)
+                        .foregroundColor( Color(red: 0.09, green: 0.63, blue: 0.52))
+                    Text(info)
+                }
+            )
+            .padding(5)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(info: "hello", iconName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
