42f9abb8b69aa85616824d4be1d2bd8252bb5f40
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2022-03-21 20:51:15 +0900

    added basic project - 2

---
+++ Basic2/Basic2/Basic2App.swift
@@ -0,0 +1,17 @@
+//
+//  Basic2App.swift
+//  Basic2
+//
+//  Created by YJ on 2022/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Basic2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Basic2/Basic2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Basic2
+//
+//  Created by YJ on 2022/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
