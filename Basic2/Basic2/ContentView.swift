19396e251523b0e477d2c11b9a76a86953f24837
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2022-03-21 21:44:46 +0900

    added basic project - 2

--- Basic2/Basic2/ContentView.swift
+++ Basic2/Basic2/ContentView.swift
@@ -9,8 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52, opacity: 1.0)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("lyj")
+                    .resizable()
+                // to keep aspect ratio
+                    .aspectRatio(nil, contentMode: .fit)
+                    .frame(width: 150, height: 120)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(.white, lineWidth: 3)
+                    )
+                
+                Text("YJ LEE")
+                    .font(Font.custom("Oxygen-Bold", size: 30))
+                    .foregroundColor(.white)
+                
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 20))
+                
+                Divider()
+                
+                InfoView(info: "+82 010-1111-2222", iconName: "phone.fill")
+                InfoView(info: "yjlee1026@yahoo.com", iconName: "envelope.fill")
+            }
+        }
+        
     }
 }
 
@@ -19,3 +47,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
